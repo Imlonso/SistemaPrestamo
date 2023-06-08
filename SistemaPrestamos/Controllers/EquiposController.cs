e854dda57ae208f67a437182c15dce459884dbea
Imlonso <2017011066@cvirtualuees.edu.sv>
2023-06-08 05:06:30 -0600

    Creacion de controladores y vistas, correccion de errores

---
+++ SistemaPrestamos/Controllers/EquiposController.cs
@@ -0,0 +1,109 @@
+﻿using Business.Interfaces;
+using Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace SistemaPrestamos.Controllers
+{
+    public class EquiposController : Controller
+    {
+        private readonly IEquipoRepository equipoRepository;
+
+        public EquiposController(IEquipoRepository equipoRepository)
+        {
+            this.equipoRepository = equipoRepository;
+        }
+
+        // GET: EntityEquipo
+        public async Task<IActionResult> Index()
+        {
+            var equipos = await equipoRepository.GetAll();
+            return View(equipos);
+        }
+
+        // GET: EntityEquipo/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var equipo = await equipoRepository.GetById(id);
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+            return View(equipo);
+        }
+
+        // GET: EntityEquipo/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EntityEquipo/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(EntityEquipo equipo)
+        {
+            if (ModelState.IsValid)
+            {
+                await equipoRepository.Add(equipo);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(equipo);
+        }
+
+        // GET: EntityEquipo/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var equipo = await equipoRepository.GetById(id);
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+            return View(equipo);
+        }
+
+        // POST: EntityEquipo/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, EntityEquipo equipo)
+        {
+            if (id != equipo.IdEquipo)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await equipoRepository.Update(equipo);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(equipo);
+        }
+
+        // GET: EntityEquipo/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var equipo = await equipoRepository.GetById(id);
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+            return View(equipo);
+        }
+
+        // POST: EntityEquipo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var equipo = await equipoRepository.GetById(id);
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+
+            await equipoRepository.Delete(equipo);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ SistemaPrestamos/Controllers/PrestamosController.cs
@@ -0,0 +1,108 @@
+﻿using Business.Interfaces;
+using Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaPrestamos.Controllers
+{
+    public class PrestamosController : Controller
+    {
+        private readonly IPrestamoRepository prestamoRepository;
+
+        public PrestamosController(IPrestamoRepository prestamoRepository)
+        {
+            this.prestamoRepository = prestamoRepository;
+        }
+
+        // GET: EntityPrestamo
+        public async Task<IActionResult> Index()
+        {
+            var prestamos = await prestamoRepository.GetAll();
+            return View(prestamos);
+        }
+
+        // GET: EntityPrestamo/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var prestamo = await prestamoRepository.GetById(id);
+            if (prestamo == null)
+            {
+                return NotFound();
+            }
+            return View(prestamo);
+        }
+
+        // GET: EntityPrestamo/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EntityPrestamo/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(EntityPrestamo prestamo)
+        {
+            if (ModelState.IsValid)
+            {
+                await prestamoRepository.Add(prestamo);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(prestamo);
+        }
+
+        // GET: EntityPrestamo/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var prestamo = await prestamoRepository.GetById(id);
+            if (prestamo == null)
+            {
+                return NotFound();
+            }
+            return View(prestamo);
+        }
+
+        // POST: EntityPrestamo/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, EntityPrestamo prestamo)
+        {
+            if (id != prestamo.IdPrestamo)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await prestamoRepository.Update(prestamo);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(prestamo);
+        }
+
+        // GET: EntityPrestamo/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var prestamo = await prestamoRepository.GetById(id);
+            if (prestamo == null)
+            {
+                return NotFound();
+            }
+            return View(prestamo);
+        }
+
+        // POST: EntityPrestamo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var prestamo = await prestamoRepository.GetById(id);
+            if (prestamo == null)
+            {
+                return NotFound();
+            }
+
+            await prestamoRepository.Delete(prestamo);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
