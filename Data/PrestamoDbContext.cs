888549254fbb6ad7470d56ce8dcb3f86959a0395
Imlonso <2017011066@cvirtualuees.edu.sv>
2023-06-07 21:42:06 -0600

    Agregando Capas, Entidades, Negocio y Datos(Creando el DbContext))

---
+++ Data/PrestamoDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class PrestamoDbContext : DbContext 
+    {
+        public DbSet<EntityMarca>? Marcas { get; set; } 
+        public DbSet<EntityPrestamo>? Prestamo { get; set;}
+        public DbSet<EntityEquipo>? Equipo { get; set;}
+
+        //Conexion a la base de datos
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server= IMLONSO\\\\LONSOSQL ; Database=DbPrestamos; User Id=sa; Password= alonso1699 \"");
+            }
+        }
+
+    }
+}
---
+++ Entities/EntityPrestamo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class EntityPrestamo
+    {
+        [Key]
+        public int IdPrestamo { get; set; }
+
+        [MaxLength(50)]
+        public string? NombrePersona { get; set; }
+
+        public DateTime? FechaInicio { get; set; }
+        public DateTime? FechaFin { get; set; }
+        public bool EstadoPrestamo { get; set; }
+
+
+
+        [ForeignKey(nameof(Marca))]
+        public int IdMarca { get; set; }
+        public EntityMarca? Marca { get; set; }
+
+
+
+    }
+    
+}
